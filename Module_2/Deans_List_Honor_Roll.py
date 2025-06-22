57501f79d77d9013a4e5490606d38e4438f33f22
jrdostal <dostal325@gmail.com>
2025-06-22 08:51:55 -0400

    built program for module 2 assignment

---
+++ Module_2/Deans_List_Honor_Roll.py
@@ -0,0 +1,31 @@
+#Deans_List_Honor_Roll.py
+#Created by: Johnathan Dostal
+#This is a program that capture input to create a new record for each student that contains the first name, last name and GPA. It will also check if the GPA is above 3.25 and print a message stating the student is on the Honor Roll. It will also check if the GPA is above 3.5 and print a message stating the student is on the Dean's List.
+
+
+student_list = []
+student_record = ""
+student_gpa = 0.0
+student_first_name = ""
+student_last_name = ""
+keep_looping = True
+
+while keep_looping == True:
+    student_last_name = input("Please enter the student's last name. \nTo exit the program, enter \"ZZZ\". \nTo list previously entered students, enter \"zzz\". ")
+    if student_last_name == "ZZZ":
+        keep_looping = False
+        print("Thank you for using the program. Goodbye!")
+    elif student_last_name == "zzz":
+        print(student_list)
+    else:
+        student_first_name = input("Please enter the student's first name: ")
+        student_gpa = float(input("Please enter the student's GPA as a decimal value: "))
+        student_record = student_last_name + "," + student_first_name + ":"+ str(student_gpa)
+        student_list.append(student_record)
+        if student_gpa >= 3.5:
+            print("Congratulations! This student has made the Dean's List!")
+        elif student_gpa >= 3.25:
+            print("Congratulations! This student has made the Honor Roll!")
+        else:
+            print("Student information entered successfully!")
+    
