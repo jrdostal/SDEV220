fe77652e825e474d67474c33fd179e7382128837
jrdostal <dostal325@gmail.com>
2025-07-23 18:51:22 -0400

    folder restructuring

--- Module_4/django_tutorial/mysite/settings.py
+++ Module_4/django_tutorial/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&&!nk+uf%5w(+2kxs9z493$l0qy*=416o-mh=pip)jgt1=j04^"
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1",".pythonanywhere.com"]
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = "Module_4/django_tutorial/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
